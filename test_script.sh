ce9cb755b67b01fa196f6ecc6d68ecc629a47ae4
hari <hari@alcfwl077.alcf.anl.gov>
2025-02-26 09:36:47 -0600

    added the config example and a test shell script

---
+++ test_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Parse options
+while getopts "h:" opt; do
+    case ${opt} in
+        h)
+            HELLO_VALUE=$OPTARG
+            ;;
+    esac
+done
+
+# If no argument is passed for -h, show usage
+if [ -z "$HELLO_VALUE" ]; then
+    echo "Error: -h option is required"
+    exit
+fi
+
+# Print the value for -h option
+echo "Hello value: $HELLO_VALUE"
+
+# Sleep for 60 seconds
+sleep 60
