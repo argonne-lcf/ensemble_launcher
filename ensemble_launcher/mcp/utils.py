6bc00424f1153081beefdf3ed00de87c64d9cc3e
harikrishna1410 <harikrishna1410@gmail.com>
2025-10-21 18:07:41 +0000

    changed example to use new tunnels

--- ensemble_launcher/mcp/utils.py
+++ ensemble_launcher/mcp/utils.py
@@ -7,8 +7,8 @@ import sys
 from typing import Optional, Tuple
 
 # --- Setup basic logging ---
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # --- Private Helper Functions ---
--- ensemble_launcher/tests/mcp_test/test_mcp_http_aurora.py
+++ ensemble_launcher/tests/mcp_test/test_mcp_http_aurora.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
+from ensemble_launcher.mcp import start_tunnel, stop_tunnel
 
 
 
@@ -31,10 +32,12 @@ client = MultiServerMCPClient(
 async def main():
     tools = await client.get_tools()
     agent = create_react_agent(llm,tools=tools)
-    agent_response = await agent.ainvoke({"messages": "Run 100 CFD simulations for a range of temperature (300 to 2000 K) and pressure (1 to 200 atm) using the tools available. You can simply distribute them linearly"})
+    agent_response = await agent.ainvoke({"messages": "Run 10 CFD simulations for a range of temperature (300 to 2000 K) and pressure (1 to 200 atm) using the tools available. You can simply distribute them linearly"})
 
     for msg in agent_response['messages']:
         msg.pretty_print()
 
 if __name__ == "__main__":
+    ret = start_tunnel("ht1410","x4613c7s0b0n0",9276,9276)
     asyncio.run(main())
+    stop_tunnel(*ret)
