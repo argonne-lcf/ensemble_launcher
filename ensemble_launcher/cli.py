56ac538a00e5c81bfa4dfef3f1feacf10c0f559b
harikrishna1410 <harikrishna1410@gmail.com>
2026-01-08 11:34:13 -0600

    added cli

---
+++ ensemble_launcher/cli.py
@@ -0,0 +1,48 @@
+import typer
+from ensemble_launcher.config import LauncherConfig, SystemConfig
+from typing import Optional
+from ensemble_launcher import EnsembleLauncher
+
+el = typer.Typer()
+
+@el.command()
+def main(ensemble_file: str,
+         system_config_file: Optional[str] = None,
+         launcher_config_file: Optional[str] = None,
+         nodes_str: Optional[str] = None,
+         pin_resources: bool = True,
+         async_orchestrator: bool = False):
+    """
+        Launch an ensemble of tasks based on the provided configuration files.
+
+        Args:
+
+            ensemble_file (str): Path to the ensemble configuration file.
+
+            system_config_file (Optional[str]): Path to the system configuration JSON file.
+
+            launcher_config_file (Optional[str]): Path to the launcher configuration JSON file.
+
+            nodes_str (Optional[str]): Comma seperated string of list of compute nodes to use.
+
+            pin_resources (bool): Whether to pin resources for tasks.
+
+            async_orchestrator (bool): Whether to use an asynchronous orchestrator.
+    """
+    system_config = SystemConfig.from_json(system_config_file) if system_config_file else SystemConfig(name="local")
+    launcher_config = LauncherConfig.from_json(launcher_config_file) if launcher_config_file else None
+    nodes = nodes_str.split(",") if nodes_str else None
+
+    launcher = EnsembleLauncher(
+        ensemble_file=ensemble_file,
+        system_config=system_config,
+        launcher_config=launcher_config,
+        Nodes=nodes,
+        pin_resources=pin_resources,
+        async_orchestrator=async_orchestrator
+    )
+    results = launcher.run()
+    return results
+
+if __name__ == "__main__":
+    el()
