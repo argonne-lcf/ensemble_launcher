3b24d20d3b5f62c4fbeae3ac572c192cdc465751
Riccardo Balin <riccardo.balin@gmail.com>
2026-06-18 22:41:11 +0000

    Add utility to get number of GPUs on the node and modifications to call_llm for batched inference

--- ensemble_launcher/helper_functions.py
+++ ensemble_launcher/helper_functions.py
@@ -3,6 +3,7 @@ import socket
 from typing import Dict, List, Optional, Any
 from ensemble_launcher.comm import Result
 import json
+from subprocess import check_output, DEVNULL
 
 
 
@@ -16,6 +17,65 @@ def get_nodes():
         return [line.split(".")[0] for line in lines]
     else:
         return [socket.gethostname()]
+
+
+def str_to_num(s: str) -> int | float:
+    """Return a number from of the correct type from a string
+
+    :param s: GPU ID
+    :type s: str
+    :return: number corresponding to input string
+    :rtype: int or float
+    """
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except ValueError:
+            raise ValueError("GPU mask needs to take ints or floats as the GPU IDs.") from None
+
+
+def get_gpus() -> list[int | float]:
+    """Get the number of GPUs available on the system node
+    """
+    # NVIDIA
+    try:
+        mask = os.environ.get("CUDA_VISIBLE_DEVICES", "")
+        if mask:
+            gpus = []
+            for gpu in mask.split(","):
+                gpus.append(str_to_num(gpu))
+        else:
+            output = check_output(["nvidia-smi", "-L"], stderr=DEVNULL).decode("utf-8").splitlines()
+            gpus = list(range(len(output)))
+        return gpus
+    except:
+        pass
+
+    # Intel
+    try:
+        mask = os.environ.get("ZE_AFFINITY_MASK", "")
+        if mask:
+            gpus = []
+            for gpu in mask.split(","):
+                gpus.append(str_to_num(gpu))
+        else:
+            output = check_output(["xpu-smi", "discovery"], stderr=DEVNULL).decode("utf-8").splitlines()
+            hierarchy_mode = os.environ.get("ZE_FLAT_DEVICE_HIERARCHY", "FLAT")
+            gpus = []
+            for i in range(len(output)):
+                if hierarchy_mode == "FLAT":
+                    gpus.append(i * 2)
+                    gpus.append(i * 2 + 1)
+                elif hierarchy_mode == "COMPOSITE":
+                    gpus.append(i + 0.0)
+                    gpus.append(i + 0.1)
+        return gpus
+    except:
+        pass
+
+    return []
 
 
 def write_results_to_json(results: Result, fname: str = "./results.json"):
